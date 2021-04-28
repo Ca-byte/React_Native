2c5d482fbb1422014a5eab452289fcd3323b113c
Caroline Vieira <carolinesanvieira@gmail.com>
2021-04-27 21:06:13 -0300

    My first commit

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import starwar from './assets/starwar.png'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+
+export default class MyFetchly extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+  
+
+  loadStarWar = () => {
+
+    fetch('https://swapi.dev/api/people/?')
+      .then( res => res.json() )
+      .then( res => {
+        this.setState({
+          data: res.people || []
+        })
+      })
+
+
+  }
+
+  componentDidMount(){
+    
+    this.loadStarWar();
+
+  }
+  render() {
+
+    return(
+      <View style={styles.container}>
+        <Image 
+        style={{width: 280, height: 190, borderRadius: 60}}
+        source={starwar}/>
+        <Text 
+           style={styles.title}>
+           "Do. Or do not. There is no try." – Yoda
+        </Text>
+
+        <FlatList
+          data={this.state.data}
+          renderItem={({item}) => (
+            <View style={styles.container}>
+              <Text style={styles.info}>{item.name}</Text>
+              <Text style={styles.info}>{item.hair_color}</Text>
+            </View>
+          )}
+          keyExtractor={item => item.name}
+        />
+
+      </View>
+  
+    )
+  }
+     
+
 }
 
+    
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 90,
+    
   },
+  title:{
+    fontSize: 20,
+    paddingVertical: 20,
+    fontWeight: '600',
+    color: '#ffff'
+
+  },
+ info: {
+   flex: 1,
+   textAlign: 'center',
+   fontSize: 23,
+   color: 'red',
+  
+
+  },
+   
 });
+
+
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,31 @@
+/*<TextInput
+style={styles.input}
+placeholder="Enter a number"
+
+/>
+<Button 
+style={styles.Button}
+title="Check 👀" 
+onPress={() => {}} 
+/>
+<Text style={styles.title}>Who are you?</Text>*/
+
+// // //return(
+                
+// //     <View style={styles.container}>
+// //         <Image
+// //           style={{width: 280, height: 190, borderRadius: 60}}
+// //           source={starwar}
+// //         />
+        
+// //         <Text 
+// //           style={styles.title}>
+// //           "Do. Or do not. There is no try." – Yoda
+// //         </Text>
+// //         <View>
+// //           {people}
+// //         </View>
+
+// //     </View>
+// //   );
+// // }
